dbebc161b284b244a8b989afce65e5373734a269
abdWasiq <khatri.wasiq@gmail.com>
2024-05-28 11:28:07 +0500

    project init

---
+++ iWatch-swiftui/iWatch_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWatch_swiftuiApp.swift
+//  iWatch-swiftui
+//
+//  Created by Abul Jaleel on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iWatch_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
