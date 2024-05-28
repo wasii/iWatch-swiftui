21e09d89b6e6366719881585ae0216db42104e11
abdWasiq <khatri.wasiq@gmail.com>
2024-05-28 12:27:37 +0500

    watch list init

--- iWatch-swiftui/ContentView.swift
+++ iWatch-swiftui/ContentView.swift
@@ -25,6 +25,17 @@ struct HomeView: View {
         "TREND"
     ]
     
+    var watchData = [
+        Watch(id: 0, color: "Dark Black", price: "$399.99", image: "black", title: "Classic Black"),
+        Watch(id: 1, color: "Rose Gold", price: "$499.99", image: "gold", title: "Grand Gold"),
+        Watch(id: 2, color: "Red", price: "$599.99", image: "red", title: "Formal Red"),
+        Watch(id: 3, color: "Light Yellow", price: "$699.99", image: "yellow", title: "Original Yellow"),
+        Watch(id: 4, color: "White", price: "$799.99", image: "white", title: "Treand White")
+    ]
+    
+    var screenWidth = UIScreen.main.bounds.width
+    var screenHeight = UIScreen.main.bounds.height
+    
     var body: some View {
         ZStack {
             Color.black.opacity(0.06)
@@ -91,6 +102,53 @@ struct HomeView: View {
                 }
                 //MENU BAR ENDS
                 
+                //Watch Items
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 25) {
+                        ForEach(watchData) { watch in
+                            ZStack(alignment: .bottom) {
+                                
+                                Color("Color")
+                                    .frame(height: screenHeight / 3)
+                                    .cornerRadius(20)
+                                
+                                VStack(spacing: 20) {
+                                    Image(watch.image)
+                                        .resizable()
+                                        .frame(width: screenWidth / 1.7)
+                                    
+                                    HStack {
+                                        VStack(alignment: .leading, spacing: 10) {
+                                            Text(watch.title)
+                                            
+                                            Text("Apple Watch")
+                                                .fontWeight(.bold)
+                                                .font(.title)
+                                        }
+                                        .foregroundColor(.white)
+                                        
+                                        Spacer(minLength: 0)
+                                        
+                                        
+                                        Button {} label: {
+                                            Image(systemName: "plus")
+                                                .font(.title3)
+                                                .foregroundColor(.white)
+                                                .padding()
+                                                .background(Color("Color1"))
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                                .padding(.horizontal)
+                                .padding(.bottom)
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 25)
+                }
+                //Watch Items Ends
                 
                 
                 Spacer()
@@ -101,4 +159,14 @@ struct HomeView: View {
 
 #Preview {
     ContentView()
+}
+
+
+
+struct Watch: Identifiable, Hashable {
+    var id: Int
+    var color: String
+    var price: String
+    var image: String
+    var title: String
 }
