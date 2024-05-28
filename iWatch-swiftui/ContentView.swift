d99f51b8a95c9dbbe8cf3e31d017163664d22d4d
abdWasiq <khatri.wasiq@gmail.com>
2024-05-28 14:35:49 +0500

    navigation bar init

--- iWatch-swiftui/ContentView.swift
+++ iWatch-swiftui/ContentView.swift
@@ -64,93 +64,97 @@ struct HomeView: View {
                 .padding(.vertical)
                 //Navigation Bar Ends
                 
-                //Header
-                HStack {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Welcome!")
-                        Text("Discovery")
-                            .font(.title)
-                            .fontWeight(.bold)
-                    }
-                    .foregroundColor(.black)
-                    
-                    Spacer()
-                    
-                    Button{} label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title)
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        //Header
+                        HStack {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Welcome!")
+                                Text("Discovery")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                            }
                             .foregroundColor(.black)
-                    }
-                }
-                .padding(.horizontal)
-                //Header Ends
-                
-                
-                //MENU BAR
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 25) {
-                        ForEach(menu, id: \.self) { item in
-                            Button {
-                                selectedItem = item
-                            } label: {
-                                Text(item)
-                                    .foregroundColor(selectedItem == item ? Color("Color1") : Color("Color").opacity(0.3))
-                                    .fontWeight(selectedItem == item ? .semibold : .regular)
+                            
+                            Spacer()
+                            
+                            Button{} label: {
+                                Image(systemName: "magnifyingglass")
+                                    .font(.title)
+                                    .foregroundColor(.black)
                             }
                         }
-                    }
-                    .padding(.horizontal)
-                }
-                //MENU BAR ENDS
-                
-                //Watch Items
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 25) {
-                        ForEach(watchData) { watch in
-                            ZStack(alignment: .bottom) {
-                                
-                                Color("Color")
-                                    .frame(height: screenHeight / 3)
-                                    .cornerRadius(20)
-                                    .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 10)
-                                    
-                                VStack(spacing: 20) {
-                                    Image(watch.image)
-                                        .resizable()
-                                        .frame(width: screenWidth / 1.7)
-                                    
-                                    HStack {
-                                        VStack(alignment: .leading, spacing: 10) {
-                                            Text(watch.title)
-                                            
-                                            Text("Apple Watch")
-                                                .fontWeight(.bold)
-                                                .font(.title)
-                                        }
-                                        .foregroundColor(.white)
-                                        
-                                        Spacer(minLength: 0)
-                                        
-                                        
-                                        Button {} label: {
-                                            Image(systemName: "plus")
-                                                .font(.title3)
-                                                .foregroundColor(.white)
-                                                .padding()
-                                                .background(Color("Color1"))
-                                                .clipShape(Circle())
-                                        }
+                        .padding(.horizontal)
+                        //Header Ends
+                        
+                        
+                        //MENU BAR
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 25) {
+                                ForEach(menu, id: \.self) { item in
+                                    Button {
+                                        selectedItem = item
+                                    } label: {
+                                        Text(item)
+                                            .foregroundColor(selectedItem == item ? Color("Color1") : Color("Color").opacity(0.3))
+                                            .fontWeight(selectedItem == item ? .semibold : .regular)
                                     }
                                 }
-                                .padding(.horizontal)
-                                .padding(.bottom)
                             }
+                            .padding(.horizontal)
                         }
+                        //MENU BAR ENDS
+                        
+                        //Watch Items
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 25) {
+                                ForEach(watchData) { watch in
+                                    ZStack(alignment: .bottom) {
+                                        
+                                        Color("Color")
+                                            .frame(height: screenHeight / 3)
+                                            .cornerRadius(20)
+                                            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 10)
+                                            
+                                        VStack(spacing: 20) {
+                                            Image(watch.image)
+                                                .resizable()
+                                                .frame(width: screenWidth / 1.7, height: 375)
+                                            
+                                            HStack {
+                                                VStack(alignment: .leading, spacing: 10) {
+                                                    Text(watch.title)
+                                                    
+                                                    Text("Apple Watch")
+                                                        .fontWeight(.bold)
+                                                        .font(.title)
+                                                }
+                                                .foregroundColor(.white)
+                                                
+                                                Spacer(minLength: 0)
+                                                
+                                                
+                                                Button {} label: {
+                                                    Image(systemName: "plus")
+                                                        .font(.title3)
+                                                        .foregroundColor(.white)
+                                                        .padding()
+                                                        .background(Color("Color1"))
+                                                        .clipShape(Circle())
+                                                }
+                                            }
+                                        }
+                                        .padding(.horizontal)
+                                        .padding(.bottom)
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 25)
+                        }
+                        //Watch Items Ends
                     }
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 25)
                 }
-                //Watch Items Ends
                 
                 
                 HStack {
