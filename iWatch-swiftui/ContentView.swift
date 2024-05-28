8e3a5f0e1921e17566e3f145fe7b0bb0fd2a50ff
abdWasiq <khatri.wasiq@gmail.com>
2024-05-28 11:53:00 +0500

    Nav, Header, Menu baar init

--- iWatch-swiftui/ContentView.swift
+++ iWatch-swiftui/ContentView.swift
@@ -9,13 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HomeView()
+    }
+}
+
+struct HomeView: View {
+    
+    @State var selectedItem = "ALL"
+    var menu = [
+        "ALL",
+        "CLASSIC",
+        "GRAND",
+        "FORMAL",
+        "ORIGINAL",
+        "TREND"
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.06)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                //Navigation Bar
+                HStack {
+                    Button {} label: {
+                        Image(systemName: "line.horizontal.3.decrease.circle")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .padding(.horizontal)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {} label: {
+                        Image(systemName: "cart")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .padding(.horizontal)
+                    }
+                }
+                .padding(.vertical)
+                //Navigation Bar Ends
+                
+                //Header
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Welcome!")
+                        Text("Discovery")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                    .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Button{} label: {
+                        Image(systemName: "magnifyingglass")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding(.horizontal)
+                //Header Ends
+                
+                
+                //MENU BAR
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 25) {
+                        ForEach(menu, id: \.self) { item in
+                            Button {
+                                selectedItem = item
+                            } label: {
+                                Text(item)
+                                    .foregroundColor(selectedItem == item ? Color("Color1") : Color("Color").opacity(0.3))
+                                    .fontWeight(selectedItem == item ? .semibold : .regular)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                //MENU BAR ENDS
+                
+                
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
